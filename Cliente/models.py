2d8993da918a124524466720d0cfcb3f40b291c5
Erick Girón <erickchelote@gmail.com>
2024-11-23 10:35:05 -0600

    correcion de identacion en los modelos

--- Cliente/models.py
+++ Cliente/models.py
@@ -6,16 +6,15 @@ class Direccion (models.Model):
     calle=models.CharField(max_length=50)
     punto_referencia=models.CharField(max_length=50)
 
-    
-class Meta:
-            db_table='Direccion'
-            verbose_name = 'Direccion'
-            verbose_name_plural = 'Direcciones'
-            ordering=['id_direccion']
+    class Meta:
+                db_table='Direccion'
+                verbose_name = 'Direccion'
+                verbose_name_plural = 'Direcciones'
+                ordering=['id_direccion']
 
-def __str__(self):
-        texto = "{0} {1}"
-        return texto.format(self.id_direccion,self.numero_casa)
+    def __str__(self):
+                texto = "{0} {1}"
+                return texto.format(self.id_direccion,self.numero_casa)
 
 
 
@@ -28,15 +27,15 @@ class Cliente (models.Model):
         puntos=models.IntegerField()
         imagen_cliente=models.ImageField(upload_to='imagen_cliente',null=True)
 
-class Meta:
-            db_table='Cliente'
-            verbose_name = 'Cliente'
-            verbose_name_plural = 'Clientes'
-            ordering=['correo_cliente']
+        class Meta:
+                db_table='Cliente'
+                verbose_name = 'Cliente'
+                verbose_name_plural = 'Clientes'
+                ordering=['correo_cliente']
 
-def __str__(self):
-        texto = "{0} {1} {2}"
-        return texto.format(self.correo_cliente,self.id_direccion,self.nombre_cliente)  
+        def __str__(self):
+                texto = "{0} {1} {2}"
+                return texto.format(self.correo_cliente,self.id_direccion,self.nombre_cliente)  
 
 class Reclamo (models.Model):
         id_reclamo=models.AutoField(primary_key=True)
@@ -44,15 +43,15 @@ class Reclamo (models.Model):
         descripcion_reclamo=models.CharField(max_length=100)
         fecha_reclamo=models.DateField()
 
-class Meta:
-            db_table='Reclamo'
-            verbose_name = 'Reclamo'
-            verbose_name_plural = 'Reclamos'
-            ordering=['id_reclamo']
+        class Meta:
+                db_table='Reclamo'
+                verbose_name = 'Reclamo'
+                verbose_name_plural = 'Reclamos'
+                ordering=['id_reclamo']
 
-def __str__(self):
-        texto = "{0} {1}"
-        return texto.format(self.correo_cliente,self.id_reclamo)  
+        def __str__(self):
+                texto = "{0} {1}"
+                return texto.format(self.correo_cliente,self.id_reclamo)  
 
 class Pago (models.Model):
         id_pago=models.AutoField(primary_key=True)
@@ -60,15 +59,15 @@ class Pago (models.Model):
         total_pago=models.DecimalField(max_digits=8, decimal_places=2)
         tipo_pago=models.CharField(max_length=15)
 
-class Meta:
-            db_table='Pago'
-            verbose_name = 'Pago'
-            verbose_name_plural = 'Pagos'
-            ordering=['id_pago']
+        class Meta:
+                db_table='Pago'
+                verbose_name = 'Pago'
+                verbose_name_plural = 'Pagos'
+                ordering=['id_pago']
 
-def __str__(self):
-        texto = "{0} {1}"
-        return texto.format(self.correo_cliente,self.id_pago)
+        def __str__(self):
+                texto = "{0} {1}"
+                return texto.format(self.correo_cliente,self.id_pago)
 
 class Descuento (models.Model):
         id_descuento=models.AutoField(primary_key=True)
@@ -79,15 +78,15 @@ class Descuento (models.Model):
         estado_descuento=models.BooleanField(default=False)
         fecha_vencimiento=models.DateField()
 
-class Meta:
-            db_table='Descuento'
-            verbose_name = 'Descuento'
-            verbose_name_plural = 'Descuentos'
-            ordering=['id_descuento']
+        class Meta:
+                db_table='Descuento'
+                verbose_name = 'Descuento'
+                verbose_name_plural = 'Descuentos'
+                ordering=['id_descuento']
 
-def __str__(self):
-        texto = "{0} {1}"
-        return texto.format(self.correo,self.correo_cliente,self.id_descuento)
+        def __str__(self):
+                texto = "{0} {1}"
+                return texto.format(self.correo,self.correo_cliente,self.id_descuento)
 
 
 class Pedido:
@@ -99,12 +98,12 @@ class Pedido:
         estado_pedido=models.CharField(max_length=15)
         costo_pedido=models.DecimalField(max_digits=8,decimal_places=2)
 
-class Meta:
-            db_table='Pedido'
-            verbose_name = 'Pedido'
-            verbose_name_plural = 'Pedidos'
-            ordering=['id_pedido']
+        class Meta:
+                db_table='Pedido'
+                verbose_name = 'Pedido'
+                verbose_name_plural = 'Pedidos'
+                ordering=['id_pedido']
 
-def __str__(self):
-        texto = "{0} {1} {2} {3}"
-        return texto.format(self.id_pedido,self.correo,self.correo_cliente,self.id_pago)
+        def __str__(self):
+                texto = "{0} {1} {2} {3}"
+                return texto.format(self.id_pedido,self.correo,self.correo_cliente,self.id_pago)
